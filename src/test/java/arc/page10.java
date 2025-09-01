72d53e0e256fbd1c542264125838a190cac86fe2
Rayush Shrestha <rayushshrestha7@gmail.com>
2025-09-01 15:49:11 +0545

    changes

--- src/test/java/arc/page10.java
+++ src/test/java/arc/page10.java
@@ -1,20 +1,8 @@
 package arc;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class page10 {
     public static void main(String[] args) {
-    	WebDriverManager.chromedriver().setup();
-        // Tell Selenium where your Chrome profile is
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("C:\\Users\\Dell\\AppData\\Local\\Google\\Chrome\\User Data");
-        options.addArguments("profile-directory=Profile 5"); // or "Profile 1"
-
-        WebDriver driver = new ChromeDriver(options);
-        driver.get("https://www.google.com");
     }
 }
