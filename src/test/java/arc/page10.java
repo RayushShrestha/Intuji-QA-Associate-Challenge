6b31226c070263c82e8f19fd0e8951cad0404303
Rayush Shrestha <rayushshrestha7@gmail.com>
2025-09-01 16:04:18 +0545

    made main class

--- src/test/java/arc/page10.java
+++ src/test/java/arc/page10.java
@@ -1,8 +1,21 @@
 package arc;
 
+import java.time.Duration;
 
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class page10 {
     public static void main(String[] args) {
+    	WebDriverManager.chromedriver().setup();
+        ChromeDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		driver.get("https://automationexercise.com/");
+    	
     }
 }
